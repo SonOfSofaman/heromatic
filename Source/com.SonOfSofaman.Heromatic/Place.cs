4dc5d73c2ed8603813adea47ea216fc1743206b1
SonOfSofaman <sonofsofaman@gmail.com>
2014-11-09 10:51:44 -0600

    Remove using statements from Place class

--- Source/com.SonOfSofaman.Heromatic/Place.cs
+++ Source/com.SonOfSofaman.Heromatic/Place.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace com.SonOfSofaman.Heromatic
+﻿namespace com.SonOfSofaman.Heromatic
 {
 	public class Place
 	{
